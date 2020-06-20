61d743ead1cbed231419360cf6b58a79a706f096
Eduardo Vozniak <vozniak@msn.com>
2020-06-19 22:23:32 -0300

    Criação de middleware para validação do parâmetro ID do projeto. O middleware intercepta a requisição e faz a validação. Inclisão do JSON do insomnia para testes da API.

--- desafio01/index.js
+++ desafio01/index.js
@@ -3,21 +3,29 @@ const { json } = require('express');
 const app = express();
 app.use(express.json());
 
-let projetos = [ { id: "1", titulo: "Detalhe de implementação", tarefas: ["Nova tarefa"] }];
+let projetos = [{ id: "1", titulo: "Detalhe de implementação", tarefas: ["Nova tarefa"] }];
 let qtdRequisicoes = 0;
 
 app.listen('3001', () => {
     console.log('Servidor ouvindo na porta 3001');
 });
 
-function middlewareContador(req, res, next)
-{
+function mdlContador(req, res, next) {
     qtdRequisicoes++;
-    console.log(`Requisição Nº: ${ qtdRequisicoes}, Método HTTP: ${ req.method }.`);
+    console.log(`Requisição Nº: ${qtdRequisicoes}, Método HTTP: ${req.method}.`);
+    return next();
+};
+
+function mdlValidarSeProjExiste(req, res, next) {
+    const { id } = req.params;
+    if (!projetos[id]) {
+        return res.status(400).json({ erro: `O Projeto ${id} não existe.` });
+    }
     return next();
 }
 
-app.post('/projects', middlewareContador, (req, res) => {
+
+app.post('/projects', mdlContador, (req, res) => {
     const { id } = req.body;
     const { title } = req.body;
     const { tasks } = req.body;
@@ -32,11 +40,11 @@ app.post('/projects', middlewareContador, (req, res) => {
     return res.json(projeto);
 });
 
-app.get('/projects', middlewareContador, (req, res) => {
+app.get('/projects', mdlContador, (req, res) => {
     return res.json(projetos);
 });
 
-app.put('/projects/:id', middlewareContador, (req, res) => {
+app.put('/projects/:id', mdlContador, mdlValidarSeProjExiste, (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
     const { tasks } = req.body;
@@ -52,15 +60,15 @@ app.put('/projects/:id', middlewareContador, (req, res) => {
     return res.json(projeto);
 });
 
-app.delete('/projects/:id', middlewareContador, (req, res,) => {
+app.delete('/projects/:id', mdlContador, mdlValidarSeProjExiste, (req, res,) => {
     const { id } = req.params;
 
-    projetos.splice(id,1);
+    projetos.splice(id, 1);
 
     return res.send();
 });
 
-app.post('/projects/:id/tasks', middlewareContador, (req, res) => {
+app.post('/projects/:id/tasks', mdlContador, mdlValidarSeProjExiste, (req, res) => {
     const { id } = req.params;
     const { task } = req.body;
     const projeto = projetos[id];
