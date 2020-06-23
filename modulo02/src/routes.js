06106a65e6b24f9bd827735efa8dfc66d724bb9e
Eduardo Vozniak <vozniak@msn.com>
2020-06-22 21:51:07 -0300

    Migrations, configuração postgresql, e configuração do sequelize.

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,7 +1,16 @@
-import Router from 'express';
+import { Router } from 'express';
+import Usuario from './app/models/Usuario';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ mensagem: 'Olá mundo!' }));
+routes.get('/', async (req, res) => {
+	const usuario = await Usuario.create({
+		nome: 'Eduardo Vozniak',
+		email: 'evozniak1@gmail.com',
+		senha_hash: '312dcsasadSA233',
+	});
+
+	return res.json(usuario);
+});
 
 export default routes;
