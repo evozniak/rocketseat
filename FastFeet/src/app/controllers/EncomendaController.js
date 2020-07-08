8539e7a9ab5e429ab04e37fee195fb5aff12c0a1
Eduardo Vozniak <vozniak@msn.com>
2020-07-07 23:52:20 -0300

    typo

--- FastFeet/src/app/controllers/EncomendaController.js
+++ FastFeet/src/app/controllers/EncomendaController.js
@@ -35,7 +35,9 @@ class EncomendaController {
         });
 
         if (!(await esquema.isValid(req.body))) {
-            return res.status(400).json({ erro: 'Erro de validação.' });
+            return res.status(400).json({
+                erro: 'Erro de validação. Campos obrigatórios não informados.',
+            });
         }
 
         const {
